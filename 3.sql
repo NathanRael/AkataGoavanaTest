205d377fd1f9aab6748c951ca87a1483e15d01db
NathanRael <143139607+NathanRael@users.noreply.github.com>
2024-05-18 12:56:35 +0300

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,3 +1,3 @@
 --Requete
 
-SELECT DISTINCT niveau FROM resultat_examen WHERE code_matiere = "ALG" HAVING count(*) >= 30;
+SELECT DISTINCT niveau FROM resultat_examen WHERE code_matiere = "ALG" AND note >= 0 HAVING count(*) >= 30;
